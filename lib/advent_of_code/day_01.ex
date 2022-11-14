4800762237798b94c57d77b7188945aad615c013
Onno Vos <onno-vos-dev@users.noreply.github.com>
2022-11-14 22:21:55 +0100

    day1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,37 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    input = input(args)
+    count_increase(tl(input), fn (a, b) -> a > b end, {hd(input), 0})
   end
 
   def part2(args) do
+    input = input(args)
+    sums = sum_sliding_threes(input, [])
+    count_increase(tl(sums), fn (a, b) -> a > b end, {hd(sums), 0})
+  end
+
+  def input(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn a -> List.to_integer(String.to_charlist(a)) end)
+  end
+
+  def count_increase([], _fun, {_last, acc}) do
+    acc
+  end
+  def count_increase([h | t], fun, {last, acc}) do
+    case fun.(h, last) do
+      true ->
+        count_increase(t, fun, {h, acc + 1})
+      false ->
+        count_increase(t, fun, {h, acc})
+    end
+  end
+
+  def sum_sliding_threes([_, _], acc) do
+    Enum.reverse(acc)
+  end
+  def sum_sliding_threes([a,b,c | t], acc) do
+    sum_sliding_threes([b , c | t], [a + b + c | acc])
   end
 end
